ee39bb78a748a912a5ffe2b559a5f38305ad6e1b
jaber nofal <jabernofal2004@gmail.com>
2025-12-08 16:43:59 +0200

    front+back ends

---
+++ backend/src/main/java/com/vita/vitacheck/VitaCheckApplication.java
@@ -0,0 +1,13 @@
+package com.vita.vitacheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaCheckApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vita/vitacheck/VitaCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vita.vitacheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
